365b2113e24645bd298d68f41240fd004919881c
Petar9512 <petarjelicic95@gmail.com>
2024-09-14 22:15:55 +0200

    + Fakultet app

---
+++ Projekti/Fakultet/Data/FakultetContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Fakultet.Data
+{
+    public class FakultetContext : DbContext
+    {
+        public FakultetContext(DbContextOptions<FakultetContext> options) : base(options) { }
+    }
+}
---
+++ Projekti/Fakultet/Program.cs
@@ -0,0 +1,37 @@
+using Fakultet.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<FakultetContext>(opcije =>
+{
+    opcije.UseSqlServer(builder.Configuration.GetConnectionString("FakultetContext"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(o =>
+    {
+        o.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
+        o.EnableTryItOutByDefault();
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
